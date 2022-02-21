32ea1808b06007b3483a7b1a119cc765dd3c7780
sunnivaliu <99672092+sunnivaliu@users.noreply.github.com>
2022-02-21 11:19:43 -0500

    update

---
+++ main.js
@@ -0,0 +1,9 @@
+<!-- javascript -->
+<script src="main.js" type="text/javascript"></script>
+
+function addNewList() {
+    alert('hello world alert!');
+    console.log('hello world console');
+}
+
+
